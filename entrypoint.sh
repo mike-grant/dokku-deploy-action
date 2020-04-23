ef4ce10acf3d949146a1c13af2aedcc014de6d15
Mike Grant <mike-grant@users.noreply.github.com>
2020-04-23 22:10:41 +0100

    fix up env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,18 @@ echo ${DOKKU_LOCAL_BRANCH}
 # Setup the SSH environment
 mkdir -p ~/.ssh
 eval `ssh-agent -s`
-ssh-add - <<< "${INPUT_SSH_PRIVATE_KEY}"
+ssh-add - <<< "${INPUT_SSH-PRIVATE-KEY}"
 ssh-keyscan $INPUT_DOKKU_HOST >> ~/.ssh/known_hosts
 
 # Setup the git environment
-git_repo="${INPUT_DOKKU_USER}@${INPUT_DOKKU_HOST}:${INPUT_DOKKU_APP_NAME}"
+git_repo="${INPUT_DOKKU-USER}@${INPUT_DOKKU-HOST}:${INPUT_DOKKU-APP-NAME}"
 cd "${GITHUB_WORKSPACE}"
 git remote add deploy "${git_repo}"
 
 # Prepare to push to Dokku git repository
-REMOTE_REF="${DOKKU_LOCAL_BRANCH}:refs/heads/${INPUT_DOKKU_REMOTE_BRANCH}"
+REMOTE_REF="${DOKKU_LOCAL_BRANCH}:refs/heads/${INPUT_DOKKU-REMOTE-BRANCH}"
 
-GIT_COMMAND="git push deploy ${REMOTE_REF} ${INPUT_GIT_PUSH_FLAGS}"
+GIT_COMMAND="git push deploy ${REMOTE_REF} ${INPUT_GIT-PUSH-FLAGS}"
 echo "GIT_COMMAND=${GIT_COMMAND}"
 
 # Push to Dokku git repository
