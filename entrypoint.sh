d7ae79c5956682ffed284848cd5ae0c6af58ce62
Mike Grant <mike-grant@users.noreply.github.com>
2020-04-23 21:40:18 +0100

    Fix error in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,19 @@ DOKKU_LOCAL_BRANCH=${DOKKU_LOCAL_BRANCH:-$GITHUB_SHA}
 # Setup the SSH environment
 mkdir -p ~/.ssh
 eval `ssh-agent -s`
-ssh-add - <<< "$SSH_PRIVATE_KEY"
+ssh-add - <<< "${SSH_PRIVATE_KEY}"
 ssh-keyscan $DOKKU_HOST >> ~/.ssh/known_hosts
 
 # Setup the git environment
-git_repo="$DOKKU_USER@$DOKKU_HOST:$DOKKU_APP_NAME"
-cd "$GITHUB_WORKSPACE"
-git remote add deploy "$git_repo"
+git_repo="${DOKKU_USER}@${DOKKU_HOST}:${DOKKU_APP_NAME}"
+cd "${GITHUB_WORKSPACE}"
+git remote add deploy "${git_repo}"
 
 # Prepare to push to Dokku git repository
-REMOTE_REF="$DOKKU_LOCAL_BRANCH:refs/heads/$DOKKU_REMOTE_BRANCH"
+REMOTE_REF="${DOKKU_LOCAL_BRANCH}:refs/heads/${DOKKU_REMOTE_BRANCH}"
 
-GIT_COMMAND="git push deploy $REMOTE_REF $GIT_PUSH_FLAGS"
-echo "GIT_COMMAND=$GIT_COMMAND"
+GIT_COMMAND="git push deploy ${REMOTE_REF} ${GIT_PUSH_FLAGS}"
+echo "GIT_COMMAND=${GIT_COMMAND}"
 
 # Push to Dokku git repository
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $GIT_COMMAND
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" ${GIT_COMMAND}
